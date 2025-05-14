24963bdf6d65d8301dee82ba23f3cdc091ec6b28
Sam Chaudry <sam.chaudry@hotmail.co.uk>
2025-05-14 23:53:50 +0100

    Port Changes

--- index.py
+++ index.py
@@ -41,4 +41,4 @@ def analyze():
         return jsonify({"error": str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=7860)
+    app.run(host="0.0.0.0", port=5001, debug=True)
